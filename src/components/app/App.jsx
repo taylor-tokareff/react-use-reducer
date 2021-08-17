4e10b51039f27458012dee9f1cbfa113187961d0
taylor-tokareff <taylortokareff@gmail.com>
2021-08-17 12:26:21 -0700

    tests pass

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+const useRecord = (init) => {
+  const [before, setBefore] = useState([]);
+  const [current, setCurrent] = useState(init);
+  const [after, setAfter] = useState([]);
+
+  const undo = () => {
+    setAfter((after) => [current, ...after]);
+    setCurrent(before[before.length - 1]);
+    setBefore((before) => before.slice(0, -1));
+  };
+
+  const redo = () => {
+    setBefore((before) => [...before, current]);
+    setCurrent(after[0]);
+    setAfter((after) => after.slice(1));
+  };
+
+  const record = (val) => {
+    setBefore((before) => [...before, current]);
+    setCurrent(val);
+  };
+
+  return {
+    undo,
+    record,
+    redo,
+    current,
+  };
+};
+
+function App() {
+  const { current, undo, redo, record } = useRecord('#FF0000');
+
+  return (
+    <>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <input
+        data-testid="color-input"
+        type="color"
+        value={current}
+        onChange={({ target }) => record(target.value)}
+      />
+      <div
+        data-testid="display"
+        style={{ backgroundColor: current, width: '10rem', height: '10rem' }}
+      ></div>
+    </>
+  );
 }
+
+export default App;
--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-  afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+    render(<App />);
+    const display = screen.getByTestId('display');
+    const colorInput = screen.getByTestId('color-input');
+    const undoButton = screen.getByText('undo');
+    const redoButton = screen.getByText('redo');
+
+    expect(display).toHaveStyle({ backgroundColor: '#FF0000' });
+
+    fireEvent.change(colorInput, { target: { value: '#0000FF' } });
+    expect(display).toHaveStyle({ backgroundColor: '#0000FF' });
+
+    fireEvent.click(undoButton);
+    expect(display).toHaveStyle({ backgroundColor: '#FF0000' });
+
+    fireEvent.click(redoButton);
+    expect(display).toHaveStyle({ backgroundColor: '#0000FF' });
+
+
+
   });
 });
